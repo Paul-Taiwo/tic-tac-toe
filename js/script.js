a6e8ab2f8ee7a062cba155c45ef99dc8cd02ac8f
Paul Taiwo <pauladedokun2000@gmail.com>
2019-02-26 15:48:10 +0100

    Add Minimax Algorithm

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Note: Minimax algorithm according to Ahmad Abdolsaheb
+
 var origBoard;
 
 // Players
@@ -63,7 +65,10 @@ function emptySquares() {
 // Best Spot for aiPlayer
 function bestSpot() {
   // Generate a random spot for the AI to play
-  return emptySquares()[Math.floor(Math.random() * emptySquares().length)];
+  // return emptySquares()[Math.floor(Math.random() * emptySquares().length)];
+
+  // Minimax Algorithm
+  return minimax(origBoard, aiPlayer).index;
 }
 
 // Check Winner
@@ -111,4 +116,58 @@ function gameOver(gameWon) {
   }
   declareWinner(gameWon.player == huPlayer ? 'You win' : 'You Lose');
   turnClick('');
+}
+
+// Minimax algorithm Unbeatable
+
+function minimax(newBoard, player) {
+  // Check for available spots
+  var availSpots = emptySquares(newBoard);
+
+  // Check Win
+  if (checkWin(newBoard, player)) {
+    return { score: -10 };
+  } else if (checkWin(newBoard, aiPlayer)) {
+    return { score: 10 };
+  } else if (availSpots.length === 0) {
+    return { score: 0 };
+  }
+
+  var moves = [];
+  for (var i = 0; i < availSpots.length; i++){
+    var move = {};
+    move.index = newBoard[availSpots[i]];
+    newBoard[availSpots[i]] = player;
+
+    if (player == aiPlayer){
+      var result = minimax(newBoard, huPlayer);
+      move.score = result.score;
+    } else {
+      var result = minimax(newBoard, aiPlayer);
+      move.score = result.score;
+    }
+
+    newBoard[availSpots[i]] = move.index;
+    moves.push(move);
+  }
+
+  var bestMove;
+  if (player === aiPlayer) {
+    var bestScore = -10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    var bestScore = 10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+  return moves[bestMove];
 }
